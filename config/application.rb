8ed640df772d8db5e085f6243fac40d8d8d0cbb2
blackmist87 <mike.blackmist@aemail4u.com>
2014-10-09 02:10:02 -0400

    added the CRUD tests to the task_controller_spec file

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,8 @@ module Todo
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
-
+    #SILENCES the depreciation error when rspec is run
+    config.i18n.enforce_available_locales = false
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
--- spec/controller/task_controller_spec.rb
+++ spec/controller/task_controller_spec.rb
@@ -18,8 +18,50 @@ describe TaskController, type: :controller do
     end 
   end
 
+  describe "GET #index" do
+    it "grabs all of the Todo list"
+    it "displays the #index view"
+  end
+
+  describe "GET #new" do 
+    it "displays the #new view"
+    it "adds another item to the Todo list"
+  end
+
+  describe "GET #edit" do 
+    it "displays the #edit view"
+    it "allows edting of the Todo list"
+  end 
+
+  describe "POST create Todo item /task" do
+    context "with a valid item" do
+      it " adds another item to the Todo list"
+      it "redirects to the new item on Todo list"
+    end
+    
+    context "without a valid item" do 
+      it "does not add an item to Todo list"
+      it "redisplays the #new view with error shown"
+    end
+  end
+
+  describe "PUT update Todo list item /task" do
+    context "with a valid item" do 
+      it "locates the correct Todo list item"
+      it "updates the Todo list item"
+      it "redirects to the updated Todo list item"
+    end
+
+    context "without a valid item" do
+      it "locates the correct Todo list item"
+      it "does not update Todo list item"
+      it "redirects to #edit and displays error"
+    end
+  end
 
 
-
-  
+  describe "DELETE item" do 
+    it "deletes item from the Todo list"
+    it "redirects to the Todo list #index"
+  end 
 end
